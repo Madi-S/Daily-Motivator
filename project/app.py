51c341cd337d1c16fbdaae9c2c1b701c04bf7596
Madi-S <shaiken.m@mail.ru>
2023-12-30 21:21:04 +0600

    Web page is ready

--- project/app.py
+++ project/app.py
@@ -1,28 +1,23 @@
 import random
 from flask import Flask, render_template
 
-from data import get_data_in_rows
+from data import get_data_in_rows, authors
 
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 data = get_data_in_rows()
 
+
 @app.get('/')
 def home():
     random_text = random.choice(random.choice(data))
-    kwargs = {'row1': data[0], 'row2': data[1], 'row3': data[2], 'random_text':random_text}
+    random_author = random.choice(authors)
+    kwargs = {'row1': data[0], 'row2': data[1], 'row3': data[2],
+              'random_text': random_text, 'random_author': random_author}
     return render_template('home.html', **kwargs)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html', error=e), 404
-
-    
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-'flask --app app run'
--- project/data.py
+++ project/data.py
@@ -1,5 +1,9 @@
 
 
+authors = ('Однажды великий мудрец сказал:', 'Не забывай, что', 'Великие слова', 'Умные слова', 'Старайся следовать этому',
+           'Очень важно', 'Помни, что', 'Я лишь скажу', 'Правда на все времена', 'Тебе помогут эти слова')
+
+
 def get_data_in_rows() -> tuple[list[str], list[str], list[str]]:
     with open('project/data.txt', 'r', encoding='utf-8') as f:
         data = f.read().strip().split('\n')
@@ -7,13 +11,12 @@ def get_data_in_rows() -> tuple[list[str], list[str], list[str]]:
     data1 = []
     data2 = []
     data3 = []
-    
+
     for i in range(0, len(data), 3):
         data1.append(data[i])
         if i + 1 < len(data):
             data2.append(data[i + 1])
         if i + 2 < len(data):
             data3.append(data[i + 2])
-        
-        
+
     return (data1, data2, data3)
